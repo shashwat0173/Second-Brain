42dfcdb707277cd259e608db5186eeecdb028f88
Shashwat Tripathi <shashwattripathi0173@gmail.com>
2025-07-29 21:08:34 +0530

    feat: connect to db on app start, use body parser for requests, app listen on port 3000, store database connection string in .env and add .env file to .gitignore, add (build,start,dev) scripts

--- src/db.ts
+++ src/db.ts
@@ -1,8 +1,19 @@
-import { model, Schema } from "mongoose";
+import mongoose, { model, Schema } from "mongoose";
 
 const UserSchema = new Schema({
   username: { type: String, unique: true },
   password: String,
 });
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI as string);
+    console.log("Database Connection: SUCCESS");
+  } catch (err) {
+    console.log("Database Connection: FAILED", err);
+  }
+};
+
+export {connectDB};
 
 export const UserModel = model("User", UserSchema);
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
 import express from "express";
 import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import { UserModel, connectDB } from "./db";
+
+dotenv.config();
 
 const app = express();
 
-app.post("/api/v1/signup", (req, res) => {});
+// connecting to database.
+connectDB();
+
+app.use(express.json());
+
+app.post("/api/v1/signup", async (req, res) => {});
 
 app.post("/api/v1/signin", (req, res) => {});
 
@@ -17,3 +26,5 @@ app.delete("/api/v1/content", (req, res) => {});
 app.post("/api/v1/brain/share", (req, res) => {});
 
 app.get("/api/v1/brain/:shareLink", (req, res) => {});
+
+app.listen(3000);
